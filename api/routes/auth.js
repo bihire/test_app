8fce0f75d7e0dcd5255fa7369e3bc08bfa9c0a34
Christian <nshimyumukizachristian>
2021-02-10 01:50:28 +0200

    Add User Controller

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,17 @@
 import express from "express";
 import AuthanticationController from "../controllers/authentication_controller";
+import UserController from '../controllers/user_controller';
+
+import edit_validator from '../middlewares/edit_validator';
 import signupValidator from "../middlewares/authMiddlewares/signup_validator";
 import signinValidator from "../middlewares/authMiddlewares/signin_validator";
 
 const router = express.Router();
 router.post("/signup", signupValidator, AuthanticationController.register);
 router.post("/signin", signinValidator, AuthanticationController.login);
+router.get("/details", UserController.viewUsers);
+router.put("/edit/:id", edit_validator, UserController.editUser);
+
+
 
 export default router;
